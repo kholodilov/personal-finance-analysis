e4f2077eb8f5897bd3d617ac95ec4c45c0fb9907
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2015-01-08 14:29:31 +0300

    separate incomes, expenses and savings

--- format.R
+++ format.R
@@ -1,5 +1,11 @@
 load_spendee_data <- function(file_path) {
   spendee_feed <- read.delim(file_path, header=T,sep=",")
+  names(spendee_feed)<-c("Date", "Category", "Comment", "Value")
   spendee_feed$Date <- as.Date(spendee_feed$Date, format="%b %d, %Y")
-  spendee_feed
+  incomes <- subset(spendee_feed, Value > 0)
+  expenses_and_savings <- subset(spendee_feed, Value < 0)
+  expenses_and_savings$Value <- (-expenses_and_savings$Value)
+  expenses <- subset(expenses_and_savings, Category != "Savings")
+  savings <- subset(expenses_and_savings, Category == "Savings")
+  list(incomes = incomes, expenses = expenses, savings = savings)
 }
--- tests/format_tests.R
+++ tests/format_tests.R
@@ -2,5 +2,10 @@ source("format.R")
 
 test_load_spendee_data <- function() {
   spendee_data <- load_spendee_data("tests/spendee_sample.csv")
-  checkEquals(colnames(spendee_data), c("Date","Categories","Note","Value"))
+  
+  checkEquals(nrow(spendee_data$incomes), 1)
+  checkEquals(nrow(spendee_data$expenses), 4)
+  checkEquals(nrow(spendee_data$savings), 1)
+  
+  checkEquals(spendee_data$incomes[1,"Date"], as.Date("2014-03-04"))
 }
